8b75bb6b8688e5801a7811df1a31d2f17b328885
takuya-ki <taku8926@gmail.com>
2023-01-16 02:10:45 +0900

    add comments of baseOnRobotVG.py

--- onrobot_vg_control/src/onrobot_vg_control/baseOnRobotVG.py
+++ onrobot_vg_control/src/onrobot_vg_control/baseOnRobotVG.py
@@ -5,27 +5,41 @@ from onrobot_vg_control.msg import OnRobotVGInput
 
 
 class onrobotbaseVG:
-    """Base class (communication protocol agnostic) for sending commands
-       and receiving the status of the OnRobot VG gripper.
+    """ onrobotbaseVG sends commands and receives the status of VG gripper.
+
+        Attributes:
+            message (list[int]): message including commands to be sent
+
+            verifyCommand:
+                Verifies that the value of each variable satisfy its limits.
+            refreshCommand:
+                Updates the command sent during the next sendCommand() call.
     """
 
     def __init__(self):
-        # Initiate output message as an empty list
+        # Initiating output message as an empty list
         self.message = []
 
         # Note: after the instantiation,
         # a ".client" member must be added to the object
 
     def verifyCommand(self, command):
-        """Verifies that the value of each variable satisfy its limits."""
+        """ Verifies that the value of each variable satisfy its limits.
 
-        # Verify that each variable is in its correct range
+            Args:
+                command (OnRobotVGOutput): command message to be verified
+
+            Returns:
+                command (OnRobotVGOutput): verified command message
+        """
+
+        # Verifying that each variable is in its correct range
         command.rVCA = max(0, command.rVCA)
         command.rVCA = min(255, command.rVCA)
         command.rVCB = max(0, command.rVCB)
         command.rVCB = min(255, command.rVCB)
 
-        # Verify that the selected mode number is available
+        # Verifying that the selected mode number is available
         if command.rMCA not in [0x0000, 0x0100, 0x0200]:
             rospy.signal_shutdown(
                 rospy.get_name() +
@@ -37,43 +51,47 @@ class onrobotbaseVG:
                 ": Select the mode number for ch B from" +
                 "0x0000 (release), 0x0100 (grip), or 0x0200 (idle).")
 
-        # Return the modified command
+        # Returning the modified command
         return command
 
     def refreshCommand(self, command):
-        """Updates the command which will be sent
-           during the next sendCommand() call.
+        """ Updates the command sent during the next sendCommand() call.
+
+            Args:
+                command (OnRobotVGOutput): command to be refreshed
         """
 
-        # Limit the value of each variable
+        # Limiting the value of each variable
         command = self.verifyCommand(command)
 
-        # Initiate command as an empty list
+        # Initiating command as an empty list
         self.message = []
 
-        # Build the command with each output variable
+        # Building the command with each output variable
         self.message.append(command.rMCA)
         self.message.append(command.rVCA)
         self.message.append(command.rMCB)
         self.message.append(command.rVCB)
 
     def sendCommand(self):
-        """Sends the command to the Gripper."""
+        """ Sends the command to the Gripper. """
 
         self.client.sendCommand(self.message)
 
     def getStatus(self):
-        """Requests the status from the gripper and
-           return it in the OnRobotVGInput msg type.
+        """ Requests the gripper status and return OnRobotVGInput message.
+
+            Returns:
+                message (list[int]): message including commands to be sent
         """
 
-        # Acquire status from the Gripper
+        # Acquiring status from the Gripper
         status = self.client.getStatus()
 
-        # Message to output
+        # Messaging to output
         message = OnRobotVGInput()
 
-        # Assign the values to their respective variables
+        # Assignning the values to their respective variables
         message.gVCA = status[0]
         message.gVCB = status[1]
 
