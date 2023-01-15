f07bfa574cf122871e29826ed826e83b7a059f2e
takuya-ki <taku8926@gmail.com>
2023-01-15 23:42:08 +0900

    add docstrings for OnRobotRGTcpNode.py

--- onrobot_rg_control/nodes/OnRobotRGTcpNode.py
+++ onrobot_rg_control/nodes/OnRobotRGTcpNode.py
@@ -1,26 +1,42 @@
 #!/usr/bin/env python3
 
 import rospy
-import onrobot_rg_modbus_tcp.comModbusTcp
 import onrobot_rg_control.baseOnRobotRG
+import onrobot_rg_modbus_tcp.comModbusTcp
+from std_srvs.srv import Trigger, TriggerResponse
 from onrobot_rg_control.msg import OnRobotRGInput
 from onrobot_rg_control.msg import OnRobotRGOutput
-from std_srvs.srv import Trigger, TriggerResponse
 
 
 class OnRobotRGTcp:
+    """ OnRobotRGTcp connects to the gripper with Modbus/TCP.
+
+        Attributes:
+            gripper (onrobot_rg_control.baseOnRobotRG.onrobotbaseRG):
+                instance of onrobotbaseRG used for the connection establishment
+            pub (rospy.Publisher): the publisher for OnRobotRGInput
+
+            restartPowerCycle:
+                Restarts the power cycle of the gripper.
+            mainLoop:
+                Loops the sending status and command, and receiving message.
+    """
+
     def __init__(self):
         # Gripper is a RG gripper with a Modbus/TCP connection
-        self.gripper = onrobot_rg_control.baseOnRobotRG.onrobotbaseRG(gtype)
-        self.gripper.client = onrobot_rg_modbus_tcp.comModbusTcp.communication(dummy)
+        self.gripper = \
+            onrobot_rg_control.baseOnRobotRG.onrobotbaseRG(gtype)
+        self.gripper.client = \
+            onrobot_rg_modbus_tcp.comModbusTcp.communication(dummy)
 
-        # Connects to the ip address received as an argument
+        # Connecting to the ip address received as an argument
         self.gripper.client.connectToDevice(ip, port, changer_addr)
 
-        # The Gripper status is published on the topic named 'OnRobotRGInput'
-        self.pub = rospy.Publisher('OnRobotRGInput', OnRobotRGInput, queue_size=1)
+        # The Gripper status is published on the topic 'OnRobotRGInput'
+        self.pub = rospy.Publisher(
+            'OnRobotRGInput', OnRobotRGInput, queue_size=1)
 
-        # The Gripper command is received from the topic named 'OnRobotRGOutput'
+        # The Gripper command is received from the topic 'OnRobotRGOutput'
         rospy.Subscriber('OnRobotRGOutput',
                          OnRobotRGOutput,
                          self.gripper.refreshCommand)
@@ -34,6 +50,8 @@ class OnRobotRGTcp:
         self.mainLoop()
 
     def restartPowerCycle(self, request):
+        """ Restarts the power cycle of the gripper. """
+
         rospy.loginfo("Restarting the power cycle of all grippers connected.")
         self.gripper.restartPowerCycle()
         rospy.sleep(1)
@@ -42,16 +60,18 @@ class OnRobotRGTcp:
             message=None)  # TODO: implement
 
     def mainLoop(self):
+        """ Loops the sending status and command, and receiving message. """
+
         prev_msg = []
         while not rospy.is_shutdown():
-            # Get and publish the Gripper status
+            # Getting and publish the Gripper status
             status = self.gripper.getStatus()
             self.pub.publish(status)
 
             rospy.sleep(0.05)
-            # Send the most recent command
+            # Sending the most recent command
             if not int(format(status.gSTA, '016b')[-1]):  # not busy
-                if not prev_msg == self.gripper.message:       # find new message
+                if not prev_msg == self.gripper.message:  # find new message
                     rospy.loginfo(rospy.get_name()+": Sending message.")
                     self.gripper.sendCommand()
             prev_msg = self.gripper.message
